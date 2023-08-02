d2008a0f8da018664b50629326d0e16cb5b712b7
roxfernandez <roxanafernandez4270@gmail.com>
2023-08-02 15:28:12 -0300

    my first commit

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
+app.get("/",(req,res) =>
+  res.sendFile(path.join(__dirname, "views",))
+)
 
 
 app.listen(PORT, () =>
